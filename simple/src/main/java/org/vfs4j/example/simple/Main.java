7983e1f42f5556fa831d7f63293ec64084a6a00a
abarhub <abarhub@gmail.com>
2022-02-12 11:49:33 +0100

    mise la jour de la librairie vfs4j en version 0.7.3

--- simple/src/main/java/org/vfs4j/example/simple/Main.java
+++ simple/src/main/java/org/vfs4j/example/simple/Main.java
@@ -1,11 +1,11 @@
 package org.vfs4j.example.simple;
 
 import com.google.common.base.Verify;
+import io.github.abarhub.vfs.core.api.VFS4JDefaultFileManager;
+import io.github.abarhub.vfs.core.api.VFS4JFileManager;
+import io.github.abarhub.vfs.core.api.path.VFS4JPaths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.vfs.core.api.DefaultFileManager;
-import org.vfs.core.api.FileManager;
-import org.vfs.core.api.PathName;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -18,12 +18,12 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         LOGGER.info("main");
-        Path path1= Paths.get("./target/test/dir1");
+        Path path1 = Paths.get("./target/test/dir1");
         Files.createDirectories(path1);
-        Path path2= Paths.get("./target/test/dir2");
+        Path path2 = Paths.get("./target/test/dir2");
         Files.createDirectories(path2);
-        Path file1=path1.resolve("fichier1.txt");
-        Path file2=path2.resolve("fichier2.txt");
+        Path file1 = path1.resolve("fichier1.txt");
+        Path file2 = path2.resolve("fichier2.txt");
 
         Files.deleteIfExists(file1);
         Files.deleteIfExists(file2);
@@ -31,9 +31,9 @@ public class Main {
         Verify.verify(!Files.exists(file1));
         Verify.verify(!Files.exists(file2));
 
-        FileManager fileManager = DefaultFileManager.get();
-        fileManager.getCommand().createFile(new PathName("dir1","fichier1.txt"));
-        fileManager.getCommand().createFile(new PathName("dir2","fichier2.txt"));
+        VFS4JFileManager fileManager = VFS4JDefaultFileManager.get();
+        fileManager.getCommand().createFile(VFS4JPaths.get("dir1", "fichier1.txt"));
+        fileManager.getCommand().createFile(VFS4JPaths.get("dir2", "fichier2.txt"));
 
         Verify.verify(Files.exists(file1));
         Verify.verify(Files.exists(file2));
