d1486a836d098a09d1e6dd642f9d78b755c84ab9
abarhub <abarhub@gmail.com>
2022-02-12 13:59:14 +0100

    amélioration de la fonction mais et des tests

--- simple/src/main/java/org/vfs4j/example/simple/Main.java
+++ simple/src/main/java/org/vfs4j/example/simple/Main.java
@@ -1,16 +1,16 @@
 package org.vfs4j.example.simple;
 
 import com.google.common.base.Verify;
-import io.github.abarhub.vfs.core.api.VFS4JDefaultFileManager;
-import io.github.abarhub.vfs.core.api.VFS4JFileManager;
+import io.github.abarhub.vfs.core.api.VFS4JFiles;
+import io.github.abarhub.vfs.core.api.path.VFS4JPathName;
 import io.github.abarhub.vfs.core.api.path.VFS4JPaths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
 
@@ -18,27 +18,73 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         LOGGER.info("main");
-        Path path1 = Paths.get("./target/test/dir1");
-        Files.createDirectories(path1);
-        Path path2 = Paths.get("./target/test/dir2");
-        Files.createDirectories(path2);
-        Path file1 = path1.resolve("fichier1.txt");
-        Path file2 = path2.resolve("fichier2.txt");
 
-        Files.deleteIfExists(file1);
-        Files.deleteIfExists(file2);
+        int noTest = 1;
 
-        Verify.verify(!Files.exists(file1));
-        Verify.verify(!Files.exists(file2));
+        if (args != null && args.length > 0) {
+            if (Objects.equals(args[0], "test1")) {
+                noTest = 1;
+            } else if (Objects.equals(args[0], "test2")) {
+                noTest = 2;
+            }
+        }
 
-        VFS4JFileManager fileManager = VFS4JDefaultFileManager.get();
-        fileManager.getCommand().createFile(VFS4JPaths.get("dir1", "fichier1.txt"));
-        fileManager.getCommand().createFile(VFS4JPaths.get("dir2", "fichier2.txt"));
+        if (noTest == 1) {
+            test1();
+        }
 
-        Verify.verify(Files.exists(file1));
-        Verify.verify(Files.exists(file2));
+        LOGGER.info("Fin du programme");
+    }
 
-        LOGGER.info("OK");
+    private static void test1() throws IOException {
+
+        VFS4JPathName file1 = VFS4JPaths.get("dir1", "fichier1.txt");
+        VFS4JPathName file2 = VFS4JPaths.get("dir1", "fichier2.txt");
+        byte[] buf = "abc".getBytes(StandardCharsets.UTF_8);
+        VFS4JPathName file01 = VFS4JPaths.get("dir2", "fichier1.txt");
+        VFS4JPathName file02 = VFS4JPaths.get("dir2", "fichier2.txt");
+
+        // delet if exists
+        VFS4JFiles.deleteIfExists(file1);
+        VFS4JFiles.deleteIfExists(file2);
+        VFS4JFiles.deleteIfExists(file01);
+        VFS4JFiles.deleteIfExists(file02);
+
+        // check not exist
+        Verify.verify(!VFS4JFiles.exists(file1));
+        Verify.verify(!VFS4JFiles.exists(file2));
+        Verify.verify(!VFS4JFiles.exists(file01));
+        Verify.verify(!VFS4JFiles.exists(file02));
+
+        // create files
+        VFS4JFiles.createFile(file1);
+        LOGGER.info("create {}", file1);
+        VFS4JFiles.write(file2, buf);
+        LOGGER.info("create {}", file2);
+
+        // check exist
+        Verify.verify(VFS4JFiles.exists(file1));
+        LOGGER.info("file {} exists", file1);
+        Verify.verify(VFS4JFiles.exists(file2));
+        LOGGER.info("file {} exists", file2);
+        Verify.verify(!VFS4JFiles.exists(file01));
+        Verify.verify(!VFS4JFiles.exists(file02));
+
+        // copy
+        LOGGER.info("copy from {} to {}", file1, file01);
+        VFS4JFiles.copy(file1, file01);
+        LOGGER.info("copy from {} to {}", file2, file02);
+        VFS4JFiles.copy(file2, file02);
+
+        // check copy exist
+        Verify.verify(VFS4JFiles.exists(file01));
+        LOGGER.info("file {} exists", file01);
+        Verify.verify(VFS4JFiles.exists(file02));
+        LOGGER.info("file {} exists", file02);
+
+        byte[] buf2 = VFS4JFiles.readAllBytes(file02);
+        Verify.verify(Arrays.equals(buf, buf2));
+        LOGGER.info("files {} and {} are equals", file2, file02);
     }
 
 }
--- simple/src/test/java/org/vfs4j/example/simple/MainTest.java
+++ simple/src/test/java/org/vfs4j/example/simple/MainTest.java
@@ -1,5 +1,11 @@
 package org.vfs4j.example.simple;
 
+import com.google.common.base.Verify;
+import io.github.abarhub.vfs.core.api.VFS4JDefaultFileManager;
+import io.github.abarhub.vfs.core.api.VFS4JFileManager;
+import io.github.abarhub.vfs.core.api.VFS4JFiles;
+import io.github.abarhub.vfs.core.api.path.VFS4JPathName;
+import io.github.abarhub.vfs.core.api.path.VFS4JPaths;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,8 +33,59 @@ class MainTest {
     }
 
     @Test
-    void main() throws IOException {
-        LOGGER.info("main");
-        Main.main(null);
+    void test1() throws IOException{
+
+        LOGGER.info("test1");
+        Path path1 = Paths.get("./workspace/dir1");
+        Files.createDirectories(path1);
+        Path path2 = Paths.get("./workspace/dir2");
+        Files.createDirectories(path2);
+        Path file1 = path1.resolve("fichier1.txt");
+        Path file2 = path2.resolve("fichier2.txt");
+
+        Files.deleteIfExists(file1);
+        Files.deleteIfExists(file2);
+
+        Verify.verify(!Files.exists(file1));
+        Verify.verify(!Files.exists(file2));
+
+        VFS4JFileManager fileManager = VFS4JDefaultFileManager.get();
+        VFS4JPathName vfile1=VFS4JPaths.get("dir1", "fichier1.txt");
+        VFS4JPathName vfile2=VFS4JPaths.get("dir2", "fichier2.txt");
+        fileManager.getCommand().createFile(vfile1);
+        fileManager.getCommand().createFile(vfile2);
+
+        Verify.verify(Files.exists(file1));
+        Verify.verify(Files.exists(file2));
+
+        LOGGER.info("OK");
+    }
+
+    @Test
+    void test2() throws IOException{
+
+        LOGGER.info("test2");
+        Path path1 = Paths.get("./workspace/dir1");
+        Files.createDirectories(path1);
+        Path path2 = Paths.get("./workspace/dir2");
+        Files.createDirectories(path2);
+        Path file1 = path1.resolve("fichier1.txt");
+        Path file2 = path2.resolve("fichier2.txt");
+
+        Files.deleteIfExists(file1);
+        Files.deleteIfExists(file2);
+
+        Verify.verify(!Files.exists(file1));
+        Verify.verify(!Files.exists(file2));
+
+        VFS4JPathName vfile1=VFS4JPaths.get("dir1", "fichier1.txt");
+        VFS4JPathName vfile2=VFS4JPaths.get("dir2", "fichier2.txt");
+        VFS4JFiles.createFile(vfile1);
+        VFS4JFiles.createFile(vfile2);
+
+        Verify.verify(Files.exists(file1));
+        Verify.verify(Files.exists(file2));
+
+        LOGGER.info("OK");
     }
 }
